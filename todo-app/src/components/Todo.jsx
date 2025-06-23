bea6face4240b10342ffaebb47fe060e7de370fb
vandna-44 <vandnabaman732@gmail.com>
2025-06-23 18:22:23 +0530

    todo

---
+++ todo-app/src/components/Todo.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+const Todo = () => {
+  return (
+    <div className='bg-white place-self-center w-2/8 mx-w-md flex flex-col p-7 min-h-[550px] rounded-xl'>
+      
+  
+    <div className='flex item-center mt-7 gap-2'>
+      <h1 className='text-3xl font-semibold'>Todo-List</h1>
+    </div>
+
+
+    </div>
+  )
+}
+
+export default Todo
