e36b7097c791e9ad6ed35c2472f8be2667042861
vandna-44 <vandnabaman732@gmail.com>
2025-07-19 02:05:34 +0530

    .

---
+++ BMI calculator/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import BMICalculator from './components/BMICalculator'
+const App = () => {
+  return (
+    <div className='w-full h-screen bg-black flex items-center justify-center'>
+      <BMICalculator/>
+    </div>
+  )
+}
+
+export default App
---
+++ BMI calculator/src/components/BMICalculator.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const BMICalculator = () => {
+  return (
+    <>
+    <div className='max-w-[400px] w-full p-5 bg-white rounded-lg shadow-md text-center font-sans'>
+        <h1 className='text-2xl font-bold text-gray-900'>BMI Calculator</h1>
+
+    </div>
+    </>
+  )
+}
+
+export default BMICalculator
