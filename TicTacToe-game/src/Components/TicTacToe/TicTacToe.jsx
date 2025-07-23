f335dcaacc22efea2d11bb62fa252a19486cb484
vandna-44 <vandnabaman732@gmail.com>
2025-07-24 02:01:58 +0530

    UI

--- TicTacToe-game/src/Components/TicTacToe/TicTacToe.jsx
+++ TicTacToe-game/src/Components/TicTacToe/TicTacToe.jsx
@@ -1,10 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './TicTacToe.css'
+import circle_icon from '../Assets/circle.png'
+import cross_icon from '../Assets/cross.png'
+
+let data = ["","","","","","","","",""]
 
 const TicTacToe = () => {
+
+  let [count, setCount] = useState(0);
+  let [lock, setLock] = useState(false);
+
+  const toggle = (w,num) =>{
+
+  }
+
+
   return (
-    <div>
-      
+    <div className='container'>
+      <h1 className='tittle'>Tic Tac Toe Game In <span>React</span> </h1>
+      <div className='board'>
+        <div className='row1'>
+          <div className="boxes"></div>
+          <div className="boxes"></div>
+          <div className="boxes"></div>
+        </div>
+        <div className='row2'>
+          <div className="boxes"></div>
+          <div className="boxes"></div>
+          <div className="boxes"></div>
+        </div>
+        <div className='row3'>
+          <div className="boxes"></div>
+          <div className="boxes"></div>
+          <div className="boxes"></div>
+        </div>
+
+
+      </div>
+      <button className="reset">Reset</button>
     </div>
   )
 }
